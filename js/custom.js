c53c23b2cebb3f87f83446e69f898347bd3b41cd
ATantasheva <a4ypaxoid@yandex.ru>
2023-01-30 12:51:22 +0300

    animation

--- js/custom.js
+++ js/custom.js
@@ -252,3 +252,20 @@ responsive: [ //адаптив
 ],
    });
    });
+//анимация блока
+   function onEntry(entry) {
+      entry.forEach(change => {
+        if (change.isIntersecting) {
+         change.target.classList.add('element-show');
+        }
+      });
+    }
+    
+    let options = {
+      threshold: [0.5] };
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = document.querySelectorAll('.element-animation');
+    
+    for (let elm of elements) {
+      observer.observe(elm);
+    }
